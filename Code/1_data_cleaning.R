00620cd4ebd3a124b2f81505447efb26d2f36ddd
cgnguyen <cgnguyen@gmail.com>
2022-08-18 12:10:20 +0000

    Updated color scheme Added extra appendix graph
    Addded figures to github

--- Code/1_data_cleaning.R
+++ Code/1_data_cleaning.R
@@ -29,8 +29,8 @@
     filter(parl!="BT")%>%
     mutate(code_1=str_sub(code, 1,1))%>%
     mutate(code_1=as.factor(fct_recode(code_1,
-                               "Passing the Buck" = "1",
-                               "Blame Shifting" = "2",
+                               "Responsibility" = "1",
+                               "Blame" = "2",
                                "Self-Praise" = "3",
                                "Cooperation" = "4",
                                "Federalism" = "5")))
@@ -41,8 +41,8 @@
     filter(parl=="BT")%>%
     mutate(code_1=str_sub(code, 1,1))%>%
     mutate(code_1=as.factor(fct_recode(code_1,
-                                       "Passing the Buck" = "6",
-                                       "Blame Shifting" = "7",
+                                       "Responsibility" = "6",
+                                       "Blame" = "7",
                                        "Self-Praise" = "8",
                                        "Cooperation" = "9",
                                        "Federalism" = "1")))
@@ -108,102 +108,6 @@
                           "specific" = "210",
                           "diffuse" = "211"))
 
-  #Direction of blame
-  D$direction <- fct_recode(D$code_2,
-                            "Bund und Länder" = "101 Bund Länder (allgemein)",
-                            "Bund und Länder" = "101 Bund und Länder (allgemein)",
-                            "Vertikal" = "102 Bund / Bundesregierung",
-                            "Vertikal" = "103 Bundesakteure (Parteifarbe identisch)",
-                            "Vertikal" = "104 Bundesakteure (Parteifarbe abweichend)",
-                            "Horizontal" = "105 Länder (allgemein)",
-                            "Horizontal" = "106 andere Länder (allgemein)",
-                            "Horizontal" = "107 andere Länder (Parteifarbe MP identisch)",
-                            "Horizontal" = "108 andere Länder (Parteifarbe MP abweichend)",
-                            "Horizontal" = "109 Akteure in anderen Ländern (Parteifarbe identisch)",
-                            "Horizontal" = "110 Akteure in anderen Ländern (Parteifarbe abweichend)",
-                            "MPK" = "111 Bund-Länder-Konferenz / MPK / FMK",
-                            "Bund und Länder" = "201 Bund und Länder (allgemein)",
-                            "Bund" = "202 Bund / Bundesregierung",
-                            "Bund" = "203 Bundesakteure (Parteifarbe identisch)",
-                            "Bund" = "204 Bundesakteure (Parteifarbe abweichend)",
-                            "Andere Länder" = "205 Länder (allgemein)",
-                            "Andere Länder" = "206 andere Länder (allgemein)",
-                            "Andere Länder" = "207 andere Länder (Parteifarbe MP identisch)",
-                            "Andere Länder" = "208 andere Länder (Parteifarbe MP abweichend)",
-                            "Andere Länder" = "209 Akteure in anderen Ländern (Parteifarbe identisch)",
-                            "Andere Länder" = "210 Akteure in anderen Ländern (Parteifarbe abweichend)",
-                            "MPK" = "211 Bund-Länder-Konferenz / MPK / FMK",
-                            "Bund und Länder" = "401 Bund und Länder (allgemein)",
-                            "Vertikal" = "402 Bund / Bundesregierung",
-                            "Vertikal" = "403 Bundesakteure (Parteifarbe identisch)",
-                            "Vertikal" = "404 Bundesakteure (Parteifarbe abweichend)",
-                            "Horizontal" = "405 Länder (allgemein)",
-                            "Horizontal" = "406 andere Länder (allgemein)",
-                            "Horizontal" = "407 andere Länder (Parteifarbe MP identisch)",
-                            "Horizontal" = "408 andere Länder (Parteifarbe MP abweichend)",
-                            "Horizontal" = "409 Akteure in anderen Ländern (Parteifarbe identisch)",
-                            "Horizontal" = "410 Akteure in anderen Ländern (Parteifarbe abweichend)",
-                            "MPK" = "411 Bund-Länder-Konferenz / MPK / FMK"
-  )
-  
-  
-  
-  
-  ## Recoding D$code_2 into D$code_2_rec
-  D$parteifarbe <- fct_recode(D$code_2,
-                              "Keine klare \nParteizuordnung\nmöglich" = "201 Bund und Länder (allgemein)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "202 Bund / Bundesregierung",
-                              "Parteifarbe identisch" = "203 Bundesakteure (Parteifarbe identisch)",
-                              "Parteifarbe abweichend" = "204 Bundesakteure (Parteifarbe abweichend)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "205 Länder (allgemein)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "206 andere Länder (allgemein)",
-                              "Parteifarbe identisch" = "207 andere Länder (Parteifarbe MP identisch)",
-                              "Parteifarbe abweichend" = "208 andere Länder (Parteifarbe MP abweichend)",
-                              "Parteifarbe identisch" = "209 Akteure in anderen Ländern (Parteifarbe identisch)",
-                              "Parteifarbe abweichend" = "210 Akteure in anderen Ländern (Parteifarbe abweichend)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "211 Bund-Länder-Konferenz / MPK / FMK",
-                              
-                              "Keine klare \nParteizuordnung\nmöglich" = "101 Bund und Länder (allgemein)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "102 Bund / Bundesregierung",
-                              "Parteifarbe identisch" = "103 Bundesakteure (Parteifarbe identisch)",
-                              "Parteifarbe abweichend" = "104 Bundesakteure (Parteifarbe abweichend)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "105 Länder (allgemein)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "106 andere Länder (allgemein)",
-                              "Parteifarbe identisch" = "107 andere Länder (Parteifarbe MP identisch)",
-                              "Parteifarbe abweichend" = "108 andere Länder (Parteifarbe MP abweichend)",
-                              "Parteifarbe identisch" = "109 Akteure in anderen Ländern (Parteifarbe identisch)",
-                              "Parteifarbe abweichend" = "110 Akteure in anderen Ländern (Parteifarbe abweichend)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "111 Bund-Länder-Konferenz / MPK / FMK",
-                              
-                              "Keine klare \nParteizuordnung\nmöglich" = "401 Bund und Länder (allgemein)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "402 Bund / Bundesregierung",
-                              "Parteifarbe identisch" = "403 Bundesakteure (Parteifarbe identisch)",
-                              "Parteifarbe abweichend" = "404 Bundesakteure (Parteifarbe abweichend)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "405 Länder (allgemein)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "406 andere Länder (allgemein)",
-                              "Parteifarbe identisch" = "407 andere Länder (Parteifarbe MP identisch)",
-                              "Parteifarbe abweichend" = "408 andere Länder (Parteifarbe MP abweichend)",
-                              "Parteifarbe identisch" = "409 Akteure in anderen Ländern (Parteifarbe identisch)",
-                              "Parteifarbe abweichend" = "410 Akteure in anderen Ländern (Parteifarbe abweichend)",
-                              "Keine klare \nParteizuordnung\nmöglich" = "411 Bund-Länder-Konferenz / MPK / FMK")
-  
-  
-  
-  ##Replace "R" für Regierung with actual parties for now----------------
-  
-  # Add Vorgangscode as names Factor ----------------------------------------
-  ## Recoding DATA$VORGANG into DATA$VORGANG_rec
-  DATA$VORGANG <- fct_recode(DATA$VORGANG,
-                             "Aktuelle Stunde" = "1",
-                             "Regierungserklärung" = "2",
-                             "Regierungserklärung" = "3",
-                             "Fiskalische Beratung" = "4",
-                             "Beratung über Gesetzgebung" = "5",
-                             "Fragestunden" = "6",
-                             "Antrag" = "7",
-                             "Andere" = "8"
-  )
-  
   
   
   
--- Code/2_visualizations_princeton.R
+++ Code/2_visualizations_princeton.R
@@ -3,18 +3,25 @@
   library(viridis)
   library(car)
 
+
+  #Define Colors
+  colors_5<-rev(viridis(5))
+  
+  colors_3<-colors_5[3:5]
+
 #Hypothesis 1---------------------------
   #Political parties are more likely to leverage federal blame-shifting strategies if they are in government
   h1_tab<-
     D%>%
-      filter(code_1 %in% c("Passing the Buck","Blame Shifting","Self-Praise"))%>%
+      filter(code_1 %in% c("Responsibility","Blame","Self-Praise"))%>%
       group_by(party,gov_p,code_1)%>%
       summarize(N=n())%>%
       mutate(ratio = round(N / sum(N), 10),
              ratio_label= round(N / sum(N), 2))%>%
       mutate(code_1=relevel(code_1, ref="Self-Praise"))
   
-  h1_tab%>%
+  fig_1<-
+    h1_tab%>%
     filter(party!="FW")%>%
     mutate(code_1=as.factor(code_1))%>%
     mutate(label_y = cumsum(ratio) -  ratio)%>%
@@ -22,22 +29,25 @@
       geom_col(position=position_stack())+
       facet_grid(.~party)+
       theme_minimal()+
-      scale_fill_manual(values=viridis(5), name="Coded Strategies")+
+      scale_fill_manual(values=colors_3, name="Coded Strategies")+
       xlab("")+ylab("Percentage of Coded Segments")+
-      theme(legend.position="bottom")+
+      theme(legend.position="bottom",text = element_text(size=16))+
       scale_x_discrete(labels=c("O" = "Opp.", "R" = "Gov."))
+  fig_1
   
-    h1_tab%>%
-      filter(party!="FW")%>%
-      mutate(code_1=as.factor(code_1))%>%
-      mutate(label_y = cumsum(ratio) -  ratio)%>%
-      ggplot(aes(x=party ,y=ratio,fill=gov_p))+
-      geom_col(position=position_dodge2(preserve="single", width=0.2), width=0.9)+
-      facet_grid(.~code_1)+
-      theme_minimal()+
-      scale_fill_manual(values=viridis(5), name="Government or Opposition", )+
-      xlab("")+ylab("Percentage of Coded Segments")+
-      theme(legend.position="bottom")
+  ggsave(filename="./Figures/fig_1.png", fig_1,width =6, height =6, unit="in")
+  
+    # h1_tab%>%
+    #   filter(party!="FW")%>%
+    #   mutate(code_1=as.factor(code_1))%>%
+    #   mutate(label_y = cumsum(ratio) -  ratio)%>%
+    #   ggplot(aes(x=party ,y=ratio,fill=gov_p))+
+    #   geom_col(position=position_dodge2(preserve="single", width=0.2), width=0.9)+
+    #   facet_grid(.~code_1)+
+    #   theme_minimal()+
+    #   scale_fill_manual(values=viridis(5), name="Government or Opposition", )+
+    #   xlab("")+ylab("Percentage of Coded Segments")+
+    #   theme(legend.position="bottom")
 
 
 #Hypothesis 3---------------------
@@ -45,37 +55,41 @@
 
     h3_tab<-
       D_land%>%
-        filter(code_1 %in% c("Passing the Buck","Blame Shifting","Self-Praise"))%>%
-        group_by(election,code_1)%>%
+      filter(code_1 %in% c("Responsibility","Blame","Self-Praise"))%>%
+      group_by(election,code_1)%>%
         summarize(N=n())%>%
         mutate(ratio = round(N / sum(N), 10),
                ratio_label= round(N / sum(N), 2))
     
     
-    h3_tab%>%
+    fig_3<-
+      h3_tab%>%
       mutate(election=car::recode(election, "1='Election';2='No Election'"))%>%
       ggplot(aes(x=code_1 ,y=ratio,fill=election))+
       geom_col(position=position_dodge2(preserve="single", width=0.2), width=0.9)+
       theme_minimal()+
-      scale_fill_manual(values=viridis(5), name="", )+
+      scale_fill_manual(values=colors_3, name="", )+
       xlab("")+ylab("Percentage of Coded Segments")+
-      theme(legend.position="bottom")
+      theme(legend.position="bottom",text = element_text(size=18))
     
-    
+    ggsave(filename="./Figures/fig_3.png", fig_3,width =6, height =6, unit="in")
 #Hypothesis 4---------------------------------------
  #Sub-national political parties prefer to shift the blame vertically to the federal level than horizontally to other constituent units. 
     
-    D_land %>%
-      filter(code_1 %in% c("Blame Shifting","Passing the Buck"))%>%
-      group_by(direction)%>%
-      summarize(n=n())%>%
-      ggplot+
-      aes(x=direction, y=n, fill=direction)+
-      geom_col(position=position_dodge())+
-      theme_minimal()+
-      xlab("Level of Blame Assignment")+ylab("Number of Coded Statements")+
-      scale_fill_manual(values=viridis(5))+
-      theme(legend.position = "none")
+   fig_4<-
+      D_land %>%
+        filter(code_1 %in% c("Responsibility","Blame"))%>%
+        group_by(direction)%>%
+        summarize(n=n())%>%
+        ggplot+
+        aes(x=direction, y=n, fill=direction)+
+        geom_col(position=position_dodge())+
+        theme_minimal()+
+        xlab("Level of Blame Assignment")+ylab("Number of Coded Statements")+
+        scale_fill_manual(values=colors_3)+
+        theme(legend.position="bottom",text = element_text(size=18))
+    
+    ggsave(filename="./Figures/fig_4.png", fig_4,width =6, height =6, unit="in")
     
     
 #Hypothesis 5-------------------------------
@@ -87,24 +101,50 @@
 #Extra Figure - Distribution of Coded statements in general 
     D$code_1 <- D$code_1 %>%
       fct_relevel(
-        "Federalism", "Cooperation", "Self-Praise", "Passing the Buck",
-        "Blame Shifting"
+        "Federalism", "Cooperation", "Self-Praise", "Responsibility",
+        "Blame"
       )
     
     
-    D%>%
+   fig_ax_1<-
+     D%>%
       group_by(code_1)%>%
       summarize(n=n())%>%
         ggplot(aes(x=code_1,y=n, fill=code_1))+
         geom_col()+
         theme_minimal()+
-        scale_fill_manual(values=viridis(8))+
+        scale_fill_manual(values=colors_5)+
         theme(legend.position = "none")+
-        xlab("")+ylab("Absolute Count of Coded Segments")+
+        xlab("")+ylab("Absolute Count of Segments")+
         coord_flip()+
-        scale_x_discrete(labels=c("Federalism" = "Value of Federalism")
+        scale_x_discrete(labels=c("Federalism" = "Value of Federalism"))+
+        theme(legend.position="none",text = element_text(size=18))
+    
+    ggsave(filename="./Figures/fig_ax_1.png", fig_ax_1,width =6, height =6, unit="in")
     
     
     
+    h1_tab_aux<-
+      D%>%
+      group_by(party,gov_p,code_1)%>%
+      summarize(N=n())%>%
+      mutate(ratio = round(N / sum(N), 10),
+             ratio_label= round(N / sum(N), 2))
+    fig_1_aux<-
+      h1_tab_aux%>%
+      filter(party!="FW")%>%
+      mutate(code_1=as.factor(code_1))%>%
+      mutate(label_y = cumsum(ratio) -  ratio)%>%
+      ggplot(aes(x=gov_p ,y=ratio,fill=code_1))+
+      geom_col(position=position_stack())+
+      facet_grid(.~party)+
+      theme_minimal()+
+      scale_fill_manual(values=colors_5, name="Coded Strategies")+
+      xlab("")+ylab("Percentage of Coded Segments")+
+      theme(legend.position="bottom",text = element_text(size=16))+
+      scale_x_discrete(labels=c("O" = "Opp.", "R" = "Gov."))
+    
+    
+    ggsave(filename="./Figures/fig_1.png", fig_1,width =6, height =6, unit="in")
     
     
